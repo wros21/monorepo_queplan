9a4a3d128acf465520f196b3afa75be54496787e
wotzoy <wotzoy@otzoy.local>
2025-08-14 13:56:04 -0600

    Change Readme21

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "http://https://queplan-backend-416665410997.us-central1.run.app:3000/api",
+  apiUrl: "https://queplan-backend-416665410997.us-central1.run.app:3000/api",
 }
